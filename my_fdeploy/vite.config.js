360275ffdd9e521a880a9203d80951a65f3b991e
aashish.git <aashishtejwani12@gmail.com>
2024-12-04 19:52:38 +0530

    finalistghg

--- my_fdeploy/vite.config.js
+++ my_fdeploy/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'deploying-Aashish-gif/Deployment/blob/master/my_fdeploy/src/App.jsx'
   plugins: [react()],
 })
