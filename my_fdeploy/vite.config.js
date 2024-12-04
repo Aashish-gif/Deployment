1e01fd27e93347e7e7ff9dd8a69c902d4d95dc4a
aashish.git <aashishtejwani12@gmail.com>
2024-12-04 19:34:49 +0530

    added docker file

--- my_fdeploy/vite.config.js
+++ my_fdeploy/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'deploying-Aashish-gif/Deployment/blob/master/my_fdeploy/src/App.jsx'
   plugins: [react()],
 })
